15670f3ca85ec74ab9446ef789e2a095e94bb637
Eric W. Greene <eric@ericwgreene.com>
2016-12-14 15:30:14 -0800

    added code demos

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,95 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import keyMirror from 'key-mirror';
 import { createStore } from 'redux';
+
+import { BaseForm } from './components/base-form';
+
+const actionTypes = keyMirror({
+	INCREMENT: null,
+	DECREMENT: null
+});
+
+const createIncrementAction = amountToChange => ({
+	type: actionTypes.INCREMENT,
+	value: amountToChange
+});
+
+const createDecrementAction = amountToChange => ({
+	type: actionTypes.DECREMENT,
+	value: amountToChange
+});
 
 const reducer = (state = 0, action) => {
 
-	//console.log('state', state);
-	//console.log('action', action);
+	console.log(state);
 
 	switch (action.type) {
-		case 'INCREMENT':
-			return state + action.value; // producing a new state
-		case 'DECREMENT':
-			return state - action.value; // producing a new state
+		case actionTypes.INCREMENT:
+			return state + action.value; 
+		case actionTypes.DECREMENT:
+			return state - action.value; 
 		default:
-			return state; // producing a new state
+			return state; 
 	}
 
 };
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-	console.log(store.getState());
-});
+class Calculator extends BaseForm {
 
-store.dispatch({ type: 'INCREMENT', value: 1 });
-store.dispatch({ type: 'DECREMENT', value: 2 });
-store.dispatch({ type: 'INCREMENT', value: 3 });
-store.dispatch({ type: 'DECREMENT', value: 4 });
-store.dispatch({ type: 'INCREMENT', value: 5 });
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentValue: 0,
+			amountToChange: 1
+		};
+	}
+
+	componentDidMount() {
+		this.unsubscribe = this.props.store.subscribe(() => {
+			this.setState({
+				currentValue: this.props.store.getState()
+			});
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	incrementValue = () => {
+		this.props.store.dispatch(createIncrementAction(this.state.amountToChange));
+	}
+
+	decrementValue = () => {
+		this.props.store.dispatch(createDecrementAction(this.state.amountToChange));
+	}
+
+	render() {
+		return <form>
+			<span>{this.state.currentValue}</span><br />
+			<button type="button" onClick={this.incrementValue}>Increment</button>
+			<button type="button" onClick={this.decrementValue}>Decrement</button>
+			<input type="number" name="amountToChange" value={this.state.amountToChange}
+				onChange={this.onChange} />
+		</form>;
+	}
+
+}
+
+ReactDOM.render(<Calculator store={store} />, document.querySelector('main'));
 
 
+// store.subscribe(() => {
+// 	console.log(store.getState());
+// });
 
-
-// console.log([
-// 	{ type: 'INCREMENT', value: 1 }, // action
-// 	{ type: 'DECREMENT', value: 2 }, // action
-// 	{ type: 'INCREMENT', value: 3 }, // action
-// 	{ type: 'DECREMENT', value: 4 }, // action
-// 	{ type: 'INCREMENT', value: 5 }, // action
-// ].reduce( (state, action) => {
-
-// 	console.log('state', state);
-// 	console.log('action', action);
-
-// 	switch (action.type) {
-// 		case 'INCREMENT':
-// 			return state + action.value; // producing a new state
-// 		case 'DECREMENT':
-// 			return state - action.value; // producing a new state
-// 		default:
-// 			return state; // producing a new state
-// 	}
-
-// }, 0));
-
-
-
-
+// store.dispatch({ type: 'INCREMENT', value: 1 });
+// store.dispatch({ type: 'DECREMENT', value: 2 });
+// store.dispatch({ type: 'INCREMENT', value: 3 });
+// store.dispatch({ type: 'DECREMENT', value: 4 });
+// store.dispatch({ type: 'INCREMENT', value: 5 });
